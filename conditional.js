54a4ec4e19a5a8a70dc186aadbb466becc1e8250
dedenandrimaulana <dedenandrimaulana@gmail.com>
2022-07-17 11:23:08 +0700

    belajar variable

--- conditional.js
+++ conditional.js
@@ -15,17 +15,41 @@
 //     console.log('Usia selain 10 dan 12')
 // }
 
-var minuman = 'Jus';
-// .tolowercase ==> Fungsi ini untuk membuat huruf jadi kecil
+// var minuman = 'Jus';
+// // .tolowercase ==> membuat huruf jadi kecil
+// // .touppercase => membuat huruf jadi besar semua
+// // penamaan tolower and toupper setelah swith."variabel"
 
-switch(minuman.toLowerCase()){
-    case 'kopi':
-        console.log('Kopi')
-        break;
-    
-    case 'teh':
-        console.log('Teh')
-        break;
-    default:
-        console.log('Maaf lagi habis')
-}
+// switch(minuman.toLowerCase()){
+//     case 'kopi':
+//         console.log('Kopi')
+//         break;
+
+//     case 'teh':
+//         console.log('Teh')
+//         break;
+//     default:
+//         console.log('Maaf lagi habis')
+// }
+
+// var Nilai = 600;
+
+// if (Nilai <= 100 && Nilai >= 85) { console.log('Nilai A') }
+// else if (Nilai < 85 && Nilai >= 70) { console.log('Nilai B') }
+// else if (Nilai < 70 && Nilai >= 55) { console.log('Nilai C') }
+// else if (Nilai < 55 && Nilai >= 40) { console.log('Nilai C') }
+// else if (Nilai < 40 && Nilai >= 20) { console.log('Nilai D') }
+// else if (Nilai < 20 && Nilai >= 0) { console.log('Nilai E') }
+// else { console.log('Masukan Nilai 0-100') }
+
+
+// Studi case 2
+// Bilangan ganjil dan genap
+// ganjil itu tidak habis dibagi 2
+// Genap itu habis dibagi 2
+// modulus % => sisa bagi
+
+var bilangan = -99;
+if (bilangan % 2 === 0) {console.log(bilangan + ' Angka Genap')}
+else if (bilangan % 2 === 1) {console.log(bilangan + ' Angka Ganjil')}
+else {console.log('Jangan masukan angka minus(-)')}
