c311771155ea5eeb8920af399f245255b62cc746
dedenandrimaulana <dedenandrimaulana@gmail.com>
2022-07-17 10:52:05 +0700

    variable if anf switch

--- conditional.js
+++ conditional.js
@@ -1,16 +1,31 @@
 // CONDITIONAL
 
-console.log("Hello Deden")
-console.log('Hello Deden')
+// console.log("Hello Deden")
+// console.log('Hello Deden')
 
-var usia = 15;
+// var usia = 15;
 
-if(usia === 10){
-    console.log('Usia 10')
-}
-else if(usia === 12){
-    console.log('Usia 12')
-}
-else{
-    console.log('Usia selain 10 dan 12')
+// if(usia === 10){
+//     console.log('Usia 10')
+// }
+// else if(usia === 12){
+//     console.log('Usia 12')
+// }
+// else{
+//     console.log('Usia selain 10 dan 12')
+// }
+
+var minuman = 'TEH';
+// .tolowercase ==> Fungsi ini untuk membuat huruf jadi kecil
+
+switch(minuman.toLowerCase()){
+    case 'kopi':
+        console.log('Kopi')
+        break;
+    
+    case 'teh':
+        console.log('Teh')
+        break;
+    default:
+        console.log('Maaf lagi habis')
 }
