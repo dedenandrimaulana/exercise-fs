d19ca58477ac473a570566d98c7a3597a3ffcac0
dedenandrimaulana <dedenandrimaulana@gmail.com>
2022-07-17 10:35:57 +0700

    belajar var

--- conditional.js
+++ conditional.js
@@ -2,3 +2,15 @@
 
 console.log("Hello Deden")
 console.log('Hello Deden')
+
+var usia = 15;
+
+if(usia === 10){
+    console.log('Usia 10')
+}
+else if(usia === 12){
+    console.log('Usia 12')
+}
+else{
+    console.log('Usia selain 10 dan 12')
+}
