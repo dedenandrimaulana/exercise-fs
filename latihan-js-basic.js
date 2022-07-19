5557a1d6174070e4eb78e876eed4ab6a95663903
dedenandrimaulana <dedenandrimaulana@gmail.com>
2022-07-19 17:12:27 +0700

    latihan soal

---
+++ latihan-js-basic.js
@@ -0,0 +1,125 @@
+// Latihan soal
+
+// 1.Untuk menghitung massa jenis sebuah benda di perlukan rumus berikut
+//      rho = massa / volume
+// Tentukan algoritma dan pseudocode untuk menghitung rumus tersebut
+
+// algorima:
+// - Tentukan nilai masa dan volume
+// - Setelah itu buat variable massa dan volume
+
+// pseudocode:
+// jika massa = x;
+// jika volume = y;
+// maka rho = x*y;
+//      output("Masa Jenis = ", rho);
+
+
+// 2.Diketahui rumus luas permukaan tabung adalah:
+// luas_permukaan_tabung = (2 x phi x tinggi x r) + (2 x phi x r x r)
+// Tentukan algoritma dan pseudocode untuk menghitung rumus tersebut
+
+// algoritma:
+// -Tentukan nilai tinggi dan jari-jari, karena nilai phi adalah 22/7
+// -Setelah itu buat variable tinggi dan jari-jari
+
+// pseudocode:
+// jika tinggi = x;
+// jika jari = y;
+// jika phi = "2,14";
+// hitung luas 1 = 2*3,14*x*y;
+// hitung luas 2 = 2*3,14*y*y;
+// maka luas total = luas 1 + luas 2;
+//      output("Luas Permukaan Tabung adalah = ", luas_total);
+
+    //  var jari = 10;
+    //  var phi = 3.14;
+    //  var volume = 0.5*3.14*jari^3;
+    //  { console.log("Volume Stengah lingkaran jika jari-jari" + jari, "=" + volume);    
+    // }
+
+    // var nama = "Deden";
+    // {console.log("Selamat Belajar," + nama);}
+
+    // var name = "Deden";
+    // var status = true;
+    // var number;
+    //  for(var i=0; i<10; i++){
+    //     if (i === 1){
+    //         console.log(name+1+status)
+    //     }else if(i === 5){
+    //         console.log(name+5+status)
+    //     } 
+            
+    //     }
+
+    // var power = 30;
+
+    // if (power > 100){
+    //     console.log("Character is Expert")
+    // } else if (power < 100 && power >= 70) {
+    //     console.log("Character is Pro")
+    // } else if (power < 70 && power >= 50){
+    //     console.log("Character is Normal")
+    // } else if (power < 50 && power >= 30) {
+    //     console.log("Character is Basic")
+    // } else {
+    //     console.log("Character is Beginner")
+    // }
+
+    // var minuman = "kopi";
+
+    // switch (minuman.toLowerCase()){
+    //     case "sweet tea": {
+    //         console.log("You Choose Sweet tea, Thanks")
+    //     } break;
+    //     case "plain tea": {
+    //         console.log("You Choose Plain Tea, Thanks")
+    //     } break;
+    //     case "boba": {
+    //         console.log("You Choose Boba, Thanks")
+    //     } break;
+    //     case "milk coffe": {
+    //         console.log("You Choose Milk Coffe, Thanks")
+    //     } break;
+    //     case "Choco Ice": {
+    //         console.log("You Choose Choco Ice, Thanks")
+    //     } default: {
+    //         console.log("Minuman Tidak Tersedia")
+    //     } break;
+        
+    // }
+     
+
+    // var pembelian = 200000;
+    // var coupon = "free15%"
+    // var limabelas = pembelian - 15/100 * pembelian;
+    // var sepuluh = pembelian - 0.1 * pembelian;
+    // var lima = pembelian - 5/100 * pembelian;
+    // var duasetengah = pembelian - 2.5/100 * pembelian;
+
+    // switch (coupon.toLowerCase()){
+    //   case "freeongkir":
+    //     if (pembelian >= 50000){
+    //         console.log("Total pembelian"+pembelian, "dan mendapat diskon 10%. Total bayar = "+sepuluh)
+    //     } else if (pembelian < 50000 && pembelian >= 30000){
+    //         console.log("Total pembelian"+pembelian, "dan mendapat diskon 5%. Total bayar =" +lima)
+    //        }   else if (pembelian < 30000){
+    //             console.log("Total pembelian"+pembelian, "dan mendapat diskon 2,5%. Total bayar =" +duasetengah)
+    //         }
+    //         break;
+    //    case "free15%":
+    //     if (pembelian >= 30000){
+    //         console.log("Total pembelian"+pembelian, "dan mendapat diskon 15%. Total bayar =" +limabelas)
+    //     } break;
+    //     default: {
+    //         console.log("Total pembelian"+pembelian, "Maaf tidak mendapatkan diskon")
+    //     } break;
+    //     }
+             
+
+var num = 10;
+   for (var i=0; i<num; i++){
+    console.log(i)
+   }
+
